efd19ac431e25e8812a82f899c2b7e3778000a3c
José Manuel Colmenar <jmcolmenar@users.noreply.github.com>
2018-05-11 10:02:49 +0200

    GE is now working

--- src/main/java/com/engine/algorithm/SymbolicRegressionGE.java
+++ src/main/java/com/engine/algorithm/SymbolicRegressionGE.java
@@ -111,7 +111,8 @@ public class SymbolicRegressionGE extends AbstractProblemGE {
         if (Double.isNaN(fValue)) {
             solution.getObjectives().set(0, Double.POSITIVE_INFINITY);
         } else {
-            solution.getObjectives().set(0, fValue);
+            // R2 best is 1, but we are minimizing
+            solution.getObjectives().set(0, 1.0 - fValue);
         }
     }
 
